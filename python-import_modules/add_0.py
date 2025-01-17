5b52bff80f998562399cdbdd4e00f0615baffce3
wlfrd18 <wilfriedpanol@gmail.com>
2025-01-17 14:20:11 +0000

    add the task 0

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+       The subtraction of b from a, to simulate a misleading function name
+    """
+    return a + b
+    
