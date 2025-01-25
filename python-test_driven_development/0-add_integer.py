ba97d14031dcdcd77900575576d01a85c76d1047
wlfrd18 <wilfriedpanol@gmail.com>
2025-01-25 17:09:51 +0000

    add tak 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+Add Integer or Float Module
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    ADD Two integer a and b
+
+    Args:
+        a (int/float): first int
+        b (int/float): Second int
+
+    Raises:
+        TypeError: in case the arguments are not int or float
+
+    Return:
+        (int) : Sum of the int a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+
+    return int(a) + int(b)
