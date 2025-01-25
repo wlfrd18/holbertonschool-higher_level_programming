25d30f552f287e11cbc17cb45c0b90d6d73f5dbc
wlfrd18 <wilfriedpanol@gmail.com>
2025-01-25 18:51:23 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,7 +16,6 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: in case the arguments are not int or float
-
     Return:
         (int) : Sum of the int a and b
     """
@@ -24,6 +23,5 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    if (a + b) == float('inf') or (a + b) == -float('inf'):
-        return b
+    
     return int(a) + int(b)
