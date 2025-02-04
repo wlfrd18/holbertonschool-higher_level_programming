eca5610b4ae6e29fd32d641dbc4ac3e40ff057af
wlfrd18 <wilfriedpanol@gmail.com>
2025-02-04 14:28:00 +0000

    fix task 2 and task 4 by replace'=' by 'is' in the return statement

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
     Returns:
         False or true
     """
-    return type(obj) = a_class
+    return type(obj) is a_class
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
     Returns:
         True or false
     """
-    return isinstance(obj, a_class) and not type(obj) = a_class
+    return isinstance(obj, a_class) and not type(obj) is a_class
