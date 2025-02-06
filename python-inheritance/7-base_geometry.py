f3b519475d9d55e8503f4abc6bdd02212c8ab12a
wlfrd18 <wilfriedpanol@gmail.com>
2025-02-06 12:33:12 +0000

    fix the task 7 by adding the boolean condition verification

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,8 +22,9 @@ class BaseGeometry:
             TypeError: <name> must be an integer
             ValueError: <name> must be greater than 0
         """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
