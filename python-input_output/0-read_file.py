dabc04735ebd1ea5b75f46b6127048aa6d99bf86
wlfrd18 <wilfriedpanol@gmail.com>
2025-02-11 18:19:27 +0000

    fix task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Module containing read_file function"""
 
+
 def read_file(filename="")
     """The read_file function"""
 
     # openning and reading file
-    with opening(filename, 'r', enconding='UTF-8') as f:
+    with opening(filename, 'r', encoding='UTF-8') as f:
         read_data = f.read()
         print(read_data, end = "")
