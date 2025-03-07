933b3403b9bed19abcb608f3f7aa1844149e29ea
wlfrd18 <wilfriedpanol@gmail.com>
2025-03-07 20:31:32 +0000

    write the request another way for task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-
+"""
+Write a script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 import sys
 import MySQLdb
 
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    query = """SELECT * FROM states where name = '{:s}'
+            ORDER by id ASC""".format(search)
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        if r[1] == search:
+            print(r)
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    # Connect to MySQL
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-
-    # Query to select states where name matches the argument (case-sensitive)
-    query = "SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY id ASC".format(sys.argv[4])
-
-    # Execute the query
-    c.execute(query)
-
-    # Fetch all results and print them
-    states = c.fetchall()
-    for state in states:
-        print(state)
-
-    # Close cursor and database connection
-    c.close()
-    db.close()
+    main()
